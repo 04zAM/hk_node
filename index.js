2397499b531b6ef85d9cf1ad891de705e9799fef
04zAM <49489566+04zAM@users.noreply.github.com>
2021-04-29 14:20:34 -0500

    Vistas de usuario

--- index.js
+++ index.js
@@ -13,7 +13,7 @@ express()
   .get('/db', async (req, res) => {
     try {
       const client = await pool.connect();
-      const result = await client.query('SELECT * FROM cuentas;');
+      const result = await client.query('SELECT * FROM usuarios;');
       const results = { 'results': (result) ? result.rows : null};
       res.render('pages/db', results );
       client.release();
